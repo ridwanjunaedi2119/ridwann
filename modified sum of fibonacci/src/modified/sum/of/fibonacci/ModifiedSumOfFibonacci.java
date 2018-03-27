3c59c3c6c888c5226413580bfcf7af4ec13d9c54
ridwanjunaedi2119 <37769722+ridwanjunaedi2119@users.noreply.github.com>
2018-03-27 11:05:51 +0700

    Add files via upload

---
+++ modified sum of fibonacci/src/modified/sum/of/fibonacci/ModifiedSumOfFibonacci.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modified.sum.of.fibonacci;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class ModifiedSumOfFibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int a=0,b=0,hasil=1,c;
+       int d = 8;
+       int e = 0;
+   Scanner input = new Scanner (System.in);
+   System.out.print ("Masukan Deret Fibonacci  : ");
+        c = input.nextInt ();
+               
+    a=b;
+    b=hasil;
+    d=7;
+    e = a+d;
+    System.out.print(" " +e);
+        System.out.println(" ");
+ 
+    }
+    } 
+    
